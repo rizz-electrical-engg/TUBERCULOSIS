e38acccd63cb66b859599d20a86e07b59f1ce84c
Rizz <123063244+white-wolf-mia@users.noreply.github.com>
2024-09-13 20:51:34 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -80,7 +80,7 @@ class TelegraphHelper:
 
 
 telegraph = TelegraphHelper(
-    "Mirror-Leech-Telegram-Bot", "https://github.com/anasty17/mirror-leech-telegram-bot"
+    "Bhartiyee-Mirror-Leech", "https://t.me/LeechxMirrorGroup"
 )
 
 print(__name__)
